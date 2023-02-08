e912ac8048830e9d08ef4154b4b411c1cb7bcc6c
zzxprint <zzx971215@163.com>
2023-02-08 20:24:55 +0800

    样式修复

--- src/pages/mine/components/ToolsContainer.vue
+++ src/pages/mine/components/ToolsContainer.vue
@@ -2,8 +2,8 @@
   <div class="tools-container">
     <div class="tools-pad">
       <ClassificationButton class="tool-btn" text="个人信息" icon="person" icon-color="#fbc02d" width="230rpx" />
-      <navigator url="/pages/address/address">
-        <ClassificationButton class="tool-btn" text="收货地址" icon="position" icon-color="#fbc02d" width="230rpx" />
+      <navigator class="tool-btn" url="/pages/address/address">
+        <ClassificationButton text="收货地址" icon="position" icon-color="#fbc02d" width="230rpx" />
       </navigator>
       <ClassificationButton class="tool-btn" text="客服中心" icon="service" icon-color="#fbc02d" width="230rpx" />
       <ClassificationButton class="tool-btn" text="设置" icon="setting" icon-color="#fbc02d" width="230rpx" />
