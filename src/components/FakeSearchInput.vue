b45fe7d7a7df17e792aca9abd98eddfd4cf3e083
zzxprint <zzx971215@163.com>
2023-02-12 16:40:45 +0800

    分类页面布局重构

--- src/components/FakeSearchInput.vue
+++ src/components/FakeSearchInput.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="search-input" @click="toSearchPage">
+  <div
+    class="search-input"
+    :style="{
+      height: props.height,
+      borderRadius: props.height
+    }"
+    @click="toSearchPage"
+  >
     <Icon icon="search" size="40rpx" color="#fbc02d"/>
-    <div class="search-btn">搜索</div>
+    <span class="place-holder">{{ placeholder }}</span>
+    <div
+      v-show="!hideBtton"
+      class="search-btn"
+      :style="{
+        height: props.height,
+        lineHeight: props.height,
+        borderRadius: props.height
+      }"
+    >
+      搜索
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Icon from '@/components/Icon.vue'
+
+const props = defineProps({
+  placeholder: {
+    type: String,
+    default: ''
+  },
+  height: {
+    type: String,
+    default: '64rpx'
+  },
+  hideBtton: {
+    type: Boolean,
+    default: false
+  }
+})
+
+// 去往搜索页面
 const toSearchPage = () => {
   uni.navigateTo({
     url: '/pages/search/search'
@@ -22,21 +57,23 @@ const toSearchPage = () => {
   justify-content: space-between;
   padding-left: 20rpx;
   width: 100%;
-  height: 64rpx;
   border: 4rpx solid $main-color;
-  border-radius: 64rpx;
   box-sizing: border-box;
   overflow: hidden;
+  .place-holder{
+    width: 100%;
+    padding-left: 10rpx;
+    text-align: left;
+    font-size: 26rpx;
+    color: #999;
+  }
   .search-btn{
     position: absolute;
     top: -4rpx;
     right: -4rpx;
     flex-shrink: 0;
     padding: 0 30rpx;
-    height: 64rpx;
-    line-height: 64rpx;
     width: auto;
-    border-radius: 64rpx;
     font-size: 28rpx;
     color: #666;
     background: $main-color;
--- src/pages/class/class.vue
+++ src/pages/class/class.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="classification">
+  <!-- 搜索框 -->
+  <div ref="searhDom" class="search-container" :style="{ paddingTop: searchPaddingTop, paddingRight: searchPaddingRight }">
+    <FakeSearchInput placeholder="搜索商品" :height="searchHeight" :hide-btton="true" />
+  </div>
+  <div class="classification" :style="{ height: classHieght }">
     <scroll-view
       class="class-container"
       scroll-y
@@ -19,20 +23,42 @@
       <div class="class-btn">分类名称</div>
       <div class="class-btn">分类名称</div>
     </scroll-view>
-    <div class="goods-container">
+    <scroll-view
+      class="goods-container"
+      scroll-y
+    >
       <GoodListCard />
       <GoodListCard />
       <GoodListCard />
       <GoodListCard />
       <GoodListCard />
       <GoodListCard />
-    </div>
+      <GoodListCard />
+      <GoodListCard />
+    </scroll-view>
   </div>
 </template>
 
 <script setup lang="ts">
 import GoodListCard from "@/components/GoodListCard.vue"
+import FakeSearchInput from "@/components/FakeSearchInput.vue"
 import { onPullDownRefresh } from "@dcloudio/uni-app"
+import { ref, onMounted } from 'vue'
+
+// 获取右上角胶囊位置并控制搜索栏高度和位置、分类页面高度
+const searchHeight = ref('65rpx')
+const searchPaddingTop = ref('10rpx')
+const searchPaddingRight = ref('30rpx')
+const classHieght = ref('100%')
+onMounted(() => {
+  if (uni.getMenuButtonBoundingClientRect) {
+    let { top, height, left } = uni.getMenuButtonBoundingClientRect()
+    searchHeight.value = height + 'px'
+    searchPaddingTop.value = top + 'px'
+    searchPaddingRight.value = `calc(100% - ${left}px + 30rpx)`
+  }
+  classHieght.value = `calc(100% - ${searchHeight.value} - ${searchPaddingTop.value} - 10rpx)`
+})
 
 // 下拉刷新
 onPullDownRefresh(() => {
@@ -41,11 +67,24 @@ onPullDownRefresh(() => {
 })
 </script>
 
+<style>
+page{
+  height: 100%;
+}
+</style>
+
 <style lang="scss" scoped>
+.search-container{
+  width: 100%;
+  padding: 10rpx 30rpx;
+  background: #fff;
+  box-sizing: border-box;
+}
 .classification{
+  display: flex;
+  height: 100%;
   .class-container{
-    position: fixed;
-    left: 0;
+    flex-shrink: 0;
     width: 180rpx;
     height: 100%;
     background: #fff;
@@ -58,7 +97,7 @@ onPullDownRefresh(() => {
   }
   .goods-container{
     width: 100%;
-    padding-left: 180rpx;
+    height: 100%;
     box-sizing: border-box;
   }
 }
